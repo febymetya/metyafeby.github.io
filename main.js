8e9ae28e43e813744b8d73501b69ad1f98813192
febymetya <119484830+febymetya@users.noreply.github.com>
2022-12-02 12:34:07 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "50px",
+    duration: 2000,
+    reset: true,
+});
+
+sr.reveal(".hero", { delay: 200 });
+sr.reveal(".aboutUs", { delay: 200 });
+sr.reveal(".container", { delay: 200 });
+sr.reveal(".order", { delay: 200 });
+sr.reveal(".gallery", { delay: 200 });
